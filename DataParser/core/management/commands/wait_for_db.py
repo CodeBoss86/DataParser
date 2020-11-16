c833741d655fa5e7f563453c7444f7b42cb08733
CodeMasK <mattode@outlook.com>
2020-11-16 01:01:47 +0100

    increased wrapper sleep time

--- DataParser/core/management/commands/wait_for_db.py
+++ DataParser/core/management/commands/wait_for_db.py
@@ -12,7 +12,7 @@ class Command(BaseCommand):
         db_conn = None
         while not db_conn:
             try:
-                time.sleep(3)
+                time.sleep(5)
                 # get the database with keyword 'default' from settings.py
                 db_conn = connections['default']
                 # prints success messge in green
